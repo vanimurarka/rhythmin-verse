17b9bcbf6f4b3ebcf91f4350755b4cbb277b6a06
Vani Murarka <vani.murarka@gmail.com>
2019-10-29 20:28:29 +0530

    calculating maatraa for whole chars, half letter to be done

--- geet-gatiroop/hpv.v5.0.js
+++ geet-gatiroop/hpv.v5.0.js
@@ -1,31 +1,85 @@
-class classChar {
-    // member variables
-    // mainChar
-    // vowelChar
-    // consonantNumber
-    // vowelNumber
+class cChar {
+    /* MEMBER VARIABLES
+    mainChar
+    vowelChar
+    consonantNumber
+    vowelNumber*/
     constructor(mainChar) {
      this.mainChar = mainChar;
+     // this.idx = 0;
      // setConsonantNumber();
     }
     set vowel(vowelChar) {
-      this.vowelChar = vowelChar;
-      // setVowelNumber;
+		this.vowelChar = vowelChar;
+		// set Vowel Number
+		switch(vowelChar)
+		{
+		  case "अ": 
+		    this.vowelNumber = 1;
+		    break;
+		  case "आ": case "ा":
+		    this.vowelNumber = 2;
+		    break;
+		  case "इ": case "ि":
+		    this.vowelNumber = 3;
+		    break;
+		  case "ई": case "ी":
+		    this.vowelNumber = 4;
+		    break;
+		  case "उ": case "ु":
+		   this.vowelNumber = 5;
+		    break;
+		  case "ऊ": case "ू":
+		    this.vowelNumber = 6;
+		    break;
+		  case "ए": case "े":
+		    this.vowelNumber = 7;
+		    break;
+		  case "ऐ": case "ै":
+		    this.vowelNumber = 8;
+		    break;
+		  case "ओ": case "ो":
+		    this.vowelNumber = 9;
+		    break;
+		  case "औ": case "ौ":
+		    this.vowelNumber = 10;
+		    break;
+		  case "ृ":
+		    this.vowelNumber = 11;
+		    break;
+		  default:
+		    this.vowelNumber = -1;
+		}
+		// set char maatraa as per vowel
+		switch (this.vowelNumber)
+        {
+			case 1: case 3: case 5: case 11:
+				this.maatraa = 1;
+				break;
+            case 2: case 4: case 6: case 8: case 10: case 7: case 9: case 10:
+				this.maatraa = 2;
+				break;
+			default:
+				this.maatraa = 0;
+				break;
+		}
     }
     get vowel() {
     	return this.vowelChar;
     }
+    set idx(n) {
+    	this.index = n;
+    }
+    get idx() {
+    	return this.index;
+    }
 }
 
-class classLine {
+
+class cLine {
 	constructor() {
 		this.characters = new Array();
 		this.count = 0;
-	}
-	push(newChar)
-	{
-		this.characters.push(newChar);
-		this.count++;
 	}
 	get last()
 	{
@@ -34,6 +88,30 @@ class classLine {
 	set last(newChar)
 	{
 		this.characters[this.count-1] = newChar;
+	}
+	push(newChar)
+	{
+		newChar.idx = this.count;
+		this.characters.push(newChar);
+		this.count++;
+	}
+	
+}
+
+class cPoem {
+	constructor(originalText) {
+		this.originalText = originalText;
+		this.lines = new Array();
+		this.lineCount = 0;
+	}
+	pushLine(newLine)
+	{
+		this.lines.push(newLine)
+		this.lineCount++;
+	}
+	get text()
+	{
+		return this.originalText;
 	}
 }
 
@@ -44,12 +122,13 @@ function visualize()
 
 function splitNprocessPoem()
 {
-	var pom = document.getElementById("pom").value;
-    var lines = pom.split("\n");
+	const oPoem = new cPoem(document.getElementById("pom").value);
+    var lines = oPoem.text.split("\n");
     maxLineLen = lines.length;
+    debugger;
     for (i = 0; i < maxLineLen; i++) // process each line - i = line index
     {
-    	const oLine = new classLine();
+    	const oLine = new cLine();
         for (k=0;k<lines[i].length;k++) // process each char: k = char index
         {
         	charCode = lines[i].charCodeAt(k);
@@ -62,7 +141,7 @@ function splitNprocessPoem()
 			}
 			else // not maatraa - true new char
 			{
-				const oChar = new classChar(lines[i].substring(k,k+1));
+				const oChar = new cChar(lines[i].substring(k,k+1));
 				
 				// space / comma OR vowel
 	            if (((charCode == 32)||(charCode == 44)) || ((charCode >= 2309) && (charCode <= 2324)))
@@ -79,7 +158,7 @@ function splitNprocessPoem()
 	          	oLine.push(oChar);
 			}
         }
-        console.log(oLine);
+        oPoem.pushLine(oLine);
     }
-
+    console.log(oPoem);
 }
